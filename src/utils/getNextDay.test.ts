4f4f8f9a50ee4d8b2bcc98d2f3f7fd5a282a4f71
ericlent <ericquaresma@hotmail.com>
2024-09-09 09:57:46 -0300

    Agrupando testes com describe

--- src/utils/getNextDay.test.ts
+++ src/utils/getNextDay.test.ts
@@ -1,8 +1,13 @@
 import { getNextDays } from "./getNextDays";
 
-it("should return the next five days", () => {
-    const days = getNextDays();
-    console.log(days);
-    
-    expect(days).toHaveLength(5);
+describe("getNextDays", () => {
+    it("should return the next five days", () => {
+        const days = getNextDays();
+        console.log(days);
+
+        //expect(days).toHaveLength(5);
+        //expect(days.length).toEqual(5);
+        expect(days.length).toBe(5);
+
+    });
 });
